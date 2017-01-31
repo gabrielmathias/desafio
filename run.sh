85f03c992795cc57bf8884d765e8cc704d46a1d3
Gabriel Franklin Araujo Mathias <gabrielmathias@gmail.com>
2017-01-31 10:18:18 -0200

    Fixing the how to run part

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-spark-submit --class main.scala.desafio.SparkBirthdays target/scala-2.11/desafio_2.11-1.0.jar data/Mock.csv Out.json
+spark-submit --class main.scala.desafio.SparkBirthdays target/scala-2.11/desafio_2.11-1.0.jar data/Mock.csv data/Out.json
 
 echo "Results in Out.json folder!" 
 
